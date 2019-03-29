6fbf59f1e5060c105b8920c6eb92a76c70a9f3ae
Jessica Reitz <jreitz@relias.com>
2019-03-28 19:44:40 -0600

    adding session headers

--- silvercreek/silvercreek.py
+++ silvercreek/silvercreek.py
@@ -5,6 +5,19 @@ import requests, bs4, json
 
 session = requests.Session()
 session.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
+session.headers['content-type'] = 'application/json; charset=UTF-8'
+session.headers['Accept'] = 'text/javascript, text/html, application/xml, text/xml, */*'
+session.headers['accept-encoding'] = 'gzip, deflate, br'
+session.headers['accept-language'] = 'en-US,en;q=0.9'
+session.headers['cache-control'] = 'no-cache'
+session.headers['connection'] = 'keep-alive'
+session.headers['content-length'] = '542'
+session.headers['pragma'] = 'no-cache'
+session.headers['referer'] = 'https://idx.diversesolutions.com/search/1838/6069?&oref=https%3A%2F%2Fsilvercreekrealty.net%2F'
+session.headers['host'] = 'idx.diversesolutions.com'
+session.headers['X-Prototype-Version'] = '1.7'
+session.headers['X-Requested-With'] = 'XMLHttpRequest'
+session.headers['origin'] = 'https://idx.diversesolutions.com'
 
 def makeRequest(url):
     global session
